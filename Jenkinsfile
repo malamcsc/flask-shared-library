93190e6b4fe187f5f9a54746655294bae3984a59
Mohsin Alam <malamcsc@gmail.com>
2025-03-31 00:43:28 +0530

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+@Library('python-shared-library') _ 
+
+python-ci-cd{
+
+    BRANCH = "master",
+    GITHUB_REPO = '',
+    IMAGE_REPO = 'malamcsc/kubernetes_project',
+    GIT_CREDENTIAL = 'jenkins-github-token'
+}
+
---
+++ instance/config.py
@@ -0,0 +1,22 @@
+import os
+
+# Values to be used during development. Here you might specify the URI of a database sitting on localhost.
+
+# Statement for enabling the development environment
+
+DB_USER = os.environ.get("DB_USER","postgres")
+DB_PASS = os.environ.get("DB_PASS","root@123")
+#POSTGRES_URL = os.environ.get("POSTGRES_URL","localhost:5433")
+INSTANCE_HOST = os.environ.get("INSTANCE_HOST","localhost")
+DB_NAME = os.environ.get("DB_NAME","flaskdb")
+DB_PORT = os.environ.get("DB_PORT","5433")
+
+DB_URL = 'postgresql://{user}:{pw}@{host}:{port}/{db}'.format(user=DB_USER,pw=DB_PASS,host=INSTANCE_HOST,port =DB_PORT, db=DB_NAME)
+print(DB_URL)
+
+SQLALCHEMY_DATABASE_URI = DB_URL
+
+
+
+
+
